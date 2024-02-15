6b386af670c244fb99efe8759f5e4464f9fa63b3
aamaro-rss <alejandro.reyes-amaro-ext@ratpsmartsystems.com>
2024-02-15 15:00:54 +0100

    Icons and main colors

--- src/features/chat/chat-menu/chat-menu.tsx
+++ src/features/chat/chat-menu/chat-menu.tsx
@@ -2,6 +2,7 @@ import { Menu, MenuContent, MenuHeader } from "@/components/menu";
 import { FindAllChatThreadForCurrentUser } from "@/features/chat/chat-services/chat-thread-service";
 import { MenuItems } from "./menu-items";
 import { NewChat } from "./new-chat";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const ChatMenu = async () => {
   const items = await FindAllChatThreadForCurrentUser();
@@ -9,6 +10,9 @@ export const ChatMenu = async () => {
   return (
     <Menu className=" p-2">
       <MenuHeader className="justify-end">
+        <div className="flex flex-col gap-8 px-12">
+          <img src="/ratp-hd.png" className="w-36"/>
+        </div>
         <NewChat />
       </MenuHeader>
       <MenuContent>
